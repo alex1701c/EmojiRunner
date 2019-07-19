08725e2fc1c575c82e8e2d154a5bab1eb8431d2b
alex1701c <alexkp12355@gmail.com>
2019-07-19 12:45:00 +0200

    Most of GUI designed and settings are loaded

--- src/config/emojirunner_config.cpp
+++ src/config/emojirunner_config.cpp
@@ -2,6 +2,8 @@
 #include <KSharedConfig>
 #include <KPluginFactory>
 #include <krunner/abstractrunner.h>
+#include <FileReader.h>
+#include <QDebug>
 
 K_PLUGIN_FACTORY(EmojiRunnerConfigFactory, registerPlugin<EmojiRunnerConfig>("kcm_krunner_emojirunner");)
 
@@ -13,26 +15,72 @@ EmojiRunnerConfig::EmojiRunnerConfig(QWidget *parent, const QVariantList &args) 
     m_ui = new EmojiRunnerConfigForm(this);
     auto *layout = new QGridLayout(this);
     layout->addWidget(m_ui, 0, 0);
-    load();
+
+    emojiCategories = FileReader::readJSONFile();
+    config = KSharedConfig::openConfig("krunnerrc")->group("Runners").group("EmojiRunner");
+    disabledEmojis = config.readEntry("disabledCategories", "").split(";", QString::SplitBehavior::SkipEmptyParts);
+
+    std::sort(emojiCategories.begin(), emojiCategories.end(), [](EmojiCategory c1, EmojiCategory c2) -> bool {
+        if (c1.name == "Smileys & Emotion") return true;
+        return c1.name < c2.name;
+    });
 
 }
 
 void EmojiRunnerConfig::load() {
-    KCModule::load();
-    
+
+    // Load categories
+    for (const auto &category:emojiCategories) {
+        if (category.name == "Favourites") continue;
+        auto *item = new QListWidgetItem();
+        item->setText(category.name);
+        item->setFlags(item->flags() | Qt::ItemIsUserCheckable);
+        disabledEmojis.contains(category.name) ? item->setCheckState(Qt::Unchecked) : item->setCheckState(Qt::Checked);;
+        m_ui->categoryListView->addItem(item);
+    }
+
+    // Load Unicode/Ios versions
+    m_ui->unicodeComboBox->addItems(unicodeVersions);
+    if (config.readEntry("unicodeVersion").isEmpty()) m_ui->unicodeComboBox->setCurrentIndex(m_ui->unicodeComboBox->count() - 1);
+    else m_ui->unicodeComboBox->setCurrentText(config.readEntry("unicodeVersion"));
+    m_ui->iosComboBox->addItems(iosVersions);
+    if (config.readEntry("unicodeVersion").isEmpty()) m_ui->iosComboBox->setCurrentIndex(m_ui->iosComboBox->count() - 1);
+    else m_ui->iosComboBox->setCurrentText(config.readEntry("iosVersion"));
+
+    // Load favourites at top
+    QStringList favouriteNames;
+    for (const auto &category:emojiCategories) {
+        if (category.name != "Favourites") continue;
+        favouriteNames = category.emojis.keys();
+        for (const auto &emoji:category.emojis.values()) {
+            auto *item = new QListWidgetItem(emoji.emoji + " " + emoji.name);
+            item->setFlags(item->flags() | Qt::ItemIsUserCheckable);
+            item->setCheckState(Qt::Checked);
+            m_ui->favouriteListView->addItem(item);
+        }
+    }
+    // Load other emojis
+    for (const auto &category:emojiCategories) {
+        if (category.name == "Favourites") continue;
+        for (const auto &emoji:category.emojis.values()) {
+            if (favouriteNames.contains(emoji.name)) continue;
+            auto *item = new QListWidgetItem(emoji.emoji + " " + emoji.name);
+            item->setFlags(item->flags() | Qt::ItemIsUserCheckable);
+            item->setCheckState(Qt::Unchecked);
+            m_ui->favouriteListView->addItem(item);
+
+        }
+    }
+
     emit changed(false);
 }
 
 
 void EmojiRunnerConfig::save() {
-
-    KCModule::save();
-
     emit changed();
 }
 
 void EmojiRunnerConfig::defaults() {
-    
     emit changed(true);
 }
 
--- src/config/emojirunner_config.h
+++ src/config/emojirunner_config.h
@@ -4,6 +4,7 @@
 #include "ui_emojirunner_config.h"
 #include <KCModule>
 #include <KConfigCore/KConfigGroup>
+#include <EmojiCategory.h>
 
 class EmojiRunnerConfigForm : public QWidget, public Ui::EmojiRunnerConfigUi {
 Q_OBJECT
@@ -17,6 +18,15 @@ Q_OBJECT
 
 public:
     explicit EmojiRunnerConfig(QWidget *parent = nullptr, const QVariantList &args = QVariantList());
+
+    KConfigGroup config;
+
+    QList<EmojiCategory> emojiCategories;
+
+    QStringList disabledEmojis;
+
+    QStringList unicodeVersions = {"3.0", "3.2", "4.0", "4.1", "5.1", "5.2", "6.0", "6.1", "7.0", "8.0", "9.0", "11.0", "12.0"};
+    QStringList iosVersions = {"6.0", "8.3", "9.0", "9.1", "10.0", "10.2", "12.1", "13.0"};
 
 public Q_SLOTS:
 

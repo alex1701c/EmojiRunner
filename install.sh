b7a3268435588756c90d9cff2de055557341177f
alex1701c <alex1701c.dev@gmx.net>
2024-03-30 08:19:47 +0100

    Build with KF5/KF6 depending on installed KRunner version
    One might have multiple versions around, but only when having a build from source setup

--- install.sh
+++ install.sh
@@ -10,7 +10,16 @@ fi
 
 mkdir -p build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DKDE_INSTALL_QTPLUGINDIR=$(kf5-config --qt-plugins)  ..
+krunner_version=$(krunner --version | grep -oP "(?<=krunner )\d+")
+if [[ "$krunner_version" == "6" ]]; then
+    echo "Building for Plasma6"
+    BUILD_QT6_OPTION="-DBUILD_WITH_QT6=ON"
+else
+    echo "Building for Plasma5"
+    BUILD_QT6_OPTION=""
+fi
+
+cmake .. -DCMAKE_BUILD_TYPE=Release -DKDE_INSTALL_USE_QT_SYS_PATHS=ON $BUILD_QT6_OPTION
 make -j$(nproc)
 sudo make install
 
@@ -18,15 +27,6 @@ sudo make install
 if pgrep -x krunner > /dev/null
 then
     kquitapp5 krunner
-fi
-
-# If KRunner does not get started using the shortcut we have to autostart it
-krunner_version=$(krunner --version | cut -d " " -f2)
-major_version=$(echo $krunner_version | cut -d "." -f -1)
-minor_version=$(echo $krunner_version | cut -d "." -f2)
-if [[ (("$major_version" < "5")) || (("$minor_version" < "17")) ]]
-then
-    kstart5 --windowclass krunner
 fi
 
 echo "Installation finished!";

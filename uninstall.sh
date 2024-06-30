0759f2ba165668b5d842d556d3475d85e745c242
alex1701c <alex1701c.dev@gmx.net>
2024-06-30 09:38:12 +0200

    Also restart krunner on uninstall
    Thanks @veger

--- uninstall.sh
+++ uninstall.sh
@@ -3,3 +3,10 @@
 set -e
 cd build
 sudo make uninstall
+
+# KRunner needs to be restarted for the changes to be applied
+krunner_version=$(krunner --version | grep -oP "(?<=krunner )\d+")
+if pgrep -x krunner > /dev/null
+then
+    kquitapp"$krunner_version" krunner
+fi
